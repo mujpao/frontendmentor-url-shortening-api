ccd3e07254dd55f0ca92140912e7ab7bdf24d7c3
mujpao <mujpao@gmail.com>
2022-10-22 14:36:41 -0700

    Refactor intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,68 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import darkLogo from "../images/logo.svg";
 import introPhoto from "../images/illustration-working.svg";
 
-class Intro extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showNavigation: false,
-    };
-
-    this.handleClickMenu = this.handleClickMenu.bind(this);
-  }
-
-  handleClickMenu() {
-    this.setState((prevState) => ({
-      showNavigation: !prevState.showNavigation,
-    }));
-  }
-
-  render() {
-    const { showNavigation } = this.state;
-
-    return (
-      <section className="intro">
-        <header>
-          <img className="intro-logo" src={darkLogo} alt="shortly-logo" />
-          <button
-            type="button"
-            className="hamburger-menu"
-            onClick={this.handleClickMenu}
-          >
-            <div />
-            <div />
-            <div />
-          </button>
-          {showNavigation && <Navigation />}
-        </header>
-        <div className="intro-content">
-          <div className="image-container">
-            <img
-              className="intro-image"
-              src={introPhoto}
-              alt="person working"
-            />
-          </div>
-          <div className="intro-text">
-            <h1>More than just shorter links</h1>
-            <p>
-              Build your brand’s recognition and get detailed insights on how
-              your links are performing.
-            </p>
-            <button className="btn get-started" type="button">
-              Get Started
-            </button>
-          </div>
+function Intro() {
+  return (
+    <section className="intro">
+      <header>
+        <img className="intro-logo" src={darkLogo} alt="shortly-logo" />
+        <Navigation />
+      </header>
+      <div className="intro-content">
+        <div className="image-container">
+          <img className="intro-image" src={introPhoto} alt="person working" />
         </div>
-      </section>
-    );
-  }
+        <div className="intro-text">
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <button className="btn get-started" type="button">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 function Navigation() {
-  return (
+  const [showNavigation, setShowNavigation] = useState(false);
+
+  const navLinks = (
     <nav>
       <ul>
         <li>Features</li>
@@ -75,6 +44,21 @@ function Navigation() {
         <li className="sign-up">Sign Up</li>
       </ul>
     </nav>
+  );
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="hamburger-menu"
+        onClick={() => setShowNavigation(!showNavigation)}
+      >
+        <div />
+        <div />
+        <div />
+      </button>
+      {showNavigation && navLinks}
+    </div>
   );
 }
 
